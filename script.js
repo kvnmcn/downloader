a65d6aeff59e571e29661d8a847fdf18393ab18b
kvnmcn <kevinxsch@gmail.com>
2024-06-22 10:41:04 +0800

    feat: view button
    add option to view video
    optimized links for download video and view video

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputText = document.getElementById("inputText");
 const outputText = document.getElementById("outputText");
+const viewButton = document.getElementById('view');
 const extractButton = document.getElementById("extractButton");
 const clearButton = document.getElementById("clearButton");
 
@@ -9,14 +10,26 @@ extractButton.addEventListener("click", function () {
   const extractedUrl = text.match(valueRegex);
 
   if (extractedUrl) {
-    const url = extractedUrl[1].replace(/\\u0025/g, "%").replace(/\\/g, "");
+    // Can use string concatenation instead of template literal
+    const url = `${extractedUrl[1].replace(/\\u0025/g, "%").replace(/\\/g, "")}&dl=1`;
+    const downloadUrl = `${url}&dl=1`;
     const download = document.createElement('button');
     outputText.innerHTML = ""; // Clear previous output
     download.classList.add('clickable');
     download.textContent = "Download";
     download.addEventListener('click', function () {
-        window.open(url, '_blank');
+      window.open(downloadUrl, '_blank');
     });
+    const viewUrl = url.replace(/&dl=1/,"");
+    const view = document.createElement('button');
+    viewButton.innerHTML = "";
+    view.classList.add('clickable');
+    view.textContent = "Open video";
+    view.addEventListener('click', function() {
+      window.open(viewUrl, '_blank');
+    });
+    viewButton.appendChild(view);
+    
     outputText.appendChild(download);
   } else {
     outputText.innerHTML = "No URL found.";
@@ -27,5 +40,6 @@ extractButton.addEventListener("click", function () {
 clearButton.addEventListener("click", function () {
   inputText.value = "";
   outputText.innerHTML = "";
+  viewButton.innerHTML = "";
 });
 
